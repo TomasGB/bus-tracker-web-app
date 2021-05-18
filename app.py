58d767d894a49bf41508cc552a96f259c18f63b8
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2021-05-18 19:19:07 -0300

    'fix'

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import requests
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def Home():
+    return '<h1>holaaa</h1>'
+
+@app.route('/api/bus', methods=['GET'])
+def BusData():
     api_endpoint='https://www.gpsbahia.com.ar/frontend/track_data/8.json?hash=0.9738513800231414'
 
     res = requests.get(api_endpoint).json()
